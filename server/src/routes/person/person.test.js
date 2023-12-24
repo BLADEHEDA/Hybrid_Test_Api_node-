9c769691f2dfa33777966fc786c2fbcf4c2cd5cd
BLADE-HEDA <you@example.com>
2023-12-24 12:53:06 +0100

    written test to verify if the integer is of valid type , uisng the chai library of jest

--- server/src/routes/person/person.test.js
+++ server/src/routes/person/person.test.js
@@ -1,18 +1,41 @@
 const request = require('supertest');
-const app = require('../../app')
+const app = require('../../app');
+const { expect } = require('chai');
 
-describe('Test GET /persons',()=>{
-    test('should respomd with 200 success', async() => {
-      const response = await request(app)
+describe('Test GET /persons', () => {
+  test('should respond with 200 success', async () => {
+    const response = await request(app)
       .get('/persons')
-      .expect('Content-Type', /json/ )
+      .expect('Content-Type', /json/)
       .expect(200);
-    })   
+  });
 });
-describe('Test POST /persons',()=>{
-test('should respond with 200 success ', () => {
-  
-})
-test('it should catchany required ,issing properties ', () => {})
 
+describe('Test POST /persons', () => {
+  test('should respond with 201 success created', async () => {
+    const response = await request(app)
+      .post('/persons')
+      .send({
+        personName: 'Bruno',
+        age: 22, // Send age as a number
+        occupation: 'Engineer',
+        residence: 'Buea',
+      })
+      .expect('Content-Type', /json/)
+      .expect(201);
+
+    // Check if the age is of type integer
+    expect(response.body.age).to.be.a('number'); // Use Chai's typeOf method
+
+    expect(response.body).to.deep.include({
+      personName: 'Bruno',
+      age: 22,
+      occupation: 'Engineer',
+      residence: 'Buea',
+    });
+  });
+
+  test('it should catch any required missing properties', () => {
+    // Your test for missing properties goes here
+  });
 });
