acd760b6b19a2b41cb61e1e2bdeb423953dae0aa
BLADE-HEDA <you@example.com>
2024-02-13 22:39:52 +0100

    successfuly connected mongoddb to express using Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,29 @@
-const http = require('http')
+const http = require('http');
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const app = require('./app')
+const PORT = process.env.PORT || 8000;
+
+const uri = 'mongodb+srv://nkwetakembrunodudu9:E4dHQmSqWt4lSB5H@cluster0.tkhtgeq.mongodb.net/?retryWrites=true&w=majority'
+const server = http.createServer(app);
+
+async function startServer() {
+  try {
+    const conn = await mongoose.connect(uri, {
+      autoIndex: true,
+      
+    });
+    
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+    server.listen(PORT, () => {
+      console.log('Server is running on port:', PORT);
+    });
+  } catch (error) {
+    console.error(error);
+    console.log('mongodb did not connect');
+    process.exit(1);
+  }
+}
 
 
-const PORT = process.env.PORT || 8000
-
-const server = http.createServer(app)
-server.listen(PORT,()=>{
-    console.log('The prot number is:',PORT );
-} )
+startServer();
